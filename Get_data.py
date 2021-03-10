ee61b990d6c83c57ff22d8e1ffa9cd1964438c2b
hieunguyen810 <52209607+hieunguyen810@users.noreply.github.com>
2021-03-10 20:39:14 +0700

    Add files via upload

--- Get_data.py
+++ Get_data.py
@@ -1,10 +1,23 @@
 import pandas as pd
-def get_data(distance, bit_rate):
-    file_name_1 = "Full_dataset/Lorentz/%scm/Lorentz_%scm_%sk.csv" % (distance, distance, bit_rate) 
-    file_name_2 = "Full_dataset/Lorentz/%scm/Lorentz_label_%scm_%sk.csv" % (distance, distance, bit_rate) 
-    X = pd.read_csv(file_name_1, header = None)
-    X = X.values
-    y = pd.read_csv(file_name_2, header = None)
-    y = y.values
-    y = y.reshape([len(y), ])
-    return X, y
+def get_data(distance, bit_rate, mode):
+    if mode == "normal":
+        file_name_1 = "Full_dataset/Lorentz/%scm/Lorentz_%scm_%sk.csv" % (distance, distance, bit_rate) 
+        file_name_2 = "Full_dataset/Lorentz/%scm/Lorentz_label_%scm_%sk.csv" % (distance, distance, bit_rate) 
+        X1 = pd.read_csv(file_name_1, header = None)
+        X1 = X1.values
+        X2 = X1
+        y = pd.read_csv(file_name_2, header = None)
+        y = y.values
+        y = y.reshape([len(y), ])
+    else: 
+        file_name_1 = "Full_dataset/Lorentz/%scm/Lorentz_%scm_%sk.csv" % (distance, distance, bit_rate) 
+        file_name_2 = "Full_dataset/Lorentz/%scm/Lorentz_%scm_%sk_related_bit.csv" % (distance, distance, bit_rate) 
+        file_name_3 = "Full_dataset/Lorentz/%scm/Lorentz_label_%scm_%sk.csv" % (distance, distance, bit_rate) 
+        X1 = pd.read_csv(file_name_1, header = None)
+        X1 = X1.values
+        X2 = pd.read_csv(file_name_2, header = None)
+        X2 = X2.values
+        y = pd.read_csv(file_name_3, header = None)
+        y = y.values
+        y = y.reshape([len(y), ])
+    return X1, X2, y
