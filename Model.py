359d0d7a2bb59e1fe7f8998e31a2b01389ee71b8
hieunguyen810 <52209607+hieunguyen810@users.noreply.github.com>
2021-04-22 14:28:04 +0700

    Update Model.py

--- Model.py
+++ Model.py
@@ -7,9 +7,9 @@ import math
 import matplotlib.pyplot as plt
 import random
 from numpy.core.umath_tests import inner1d
-def softmax(x):
-    e_x = np.exp(x - np.max(x))
-    return e_x / e_x.sum(axis=0)
+def sigmoid(x):
+    sig = 1 / (1 + math.exp(-x))
+    return sig
 class PNN:
     def __init__(self, x_train, y_train, x_test, y_test, std):
         self.x_train = x_train
@@ -19,7 +19,7 @@ class PNN:
         self.std = std
     def activation_func(self, distances, my_class): #kernel
         kernel = np.exp((-distances)/(self.std**2))   # for dot product
-        #kernel = softmax(kernel)
+        kernel = sigmoid(kernel)
         return np.sum(kernel)
     def output(self,my_class, i):
         #distances = np.abs(self.x_test[i]-my_class) # manhattan distance  
